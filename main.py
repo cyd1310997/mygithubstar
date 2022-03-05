ab6ca1e2a31910fd3ebfbf625e7c83edc1871471
cyd1310997 <cyd1310997@qq.com>
2022-03-05 15:15:19 +0800

    03/05/22-15:15:19

--- main.py
+++ main.py
@@ -11,42 +11,17 @@ import re
 import uuid
 ######################################
 
-######################################
-from math import ceil
-from github import Github
-
-
-def starred_repos(user):
-    starred = user.get_starred()
-    total_pages = ceil(starred.totalCount / 30) 
-
-    for page_num in range(0, total_pages):
-        for repo in starred.get_page(page_num):
-            yield repo
-
-def export_github_stars():
-    gh = Github()
-    user = gh.get_user('cyd1310997')
-    L = []
-    for repo in starred_repos(user):
-        title = str(repo.description)
-        url = str(repo.html_url)
-
-        repo_name = re.sub('^.*/(.*)/(.*)$', '\\1/\\2: ', url)
-        
-        title = title.replace('`', '')
-        title = title.replace('<', '')
-        title = title.replace('>', '')
-        title = repo_name + title
-        
-        s = f'<a href="{url}" target="_blank">{title}</a>  |  <br>  '
-        L.append(s)
-    with open('nav.githubstar.html', 'w') as f:
-        f.write('\n'.join(L) + '\n')
-    shutil.copy('nav.githubstar.html','nav.githubstar.md')
 
 def main():
-    export_github_stars()
+    with open('nav.githubstar.md', 'r') as f:
+        L = f.readlines()
+    L = [i.strip() for i in L]
+    L = sorted(set(L),key=L.index)
+    L = sorted(L)
+    with open('nav.githubstar.sorted.md', 'w') as f:
+        f.write('\n'.join(L) + '\n')
     print('ok')
+
 if __name__ == '__main__':
     main()
+    
---
+++ nav.md
@@ -0,0 +1,2 @@
+[@nav.githubstar.sorted.md](nav.githubstar.sorted.md)
+[@nav.githubstar.md](nav.githubstar.md)
